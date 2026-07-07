4bdcbdce8e19ea8b90d72731088f41253628dace
lasb0001 <bolajilasisi13@gmail.com>
2026-07-07 11:08:09 +0100

    Initial QR Scanner project

---
+++ www/script.js
@@ -0,0 +1,9 @@
+const result = document.getElementById("result");
+
+document.getElementById("scanBtn").onclick = function () {
+    result.innerHTML = "📷 QR Scanner coming soon...";
+};
+
+document.getElementById("generateBtn").onclick = function () {
+    result.innerHTML = "✨ QR Generator coming soon...";
+};
